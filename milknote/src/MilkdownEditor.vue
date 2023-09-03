bda608a0b192d3cd566fd68a4bacaf34c6b9762b
Wake Liu <wake@protype.tw>
2023-09-03 22:44:11 +0800

    chore: 加掛 milkdown 基於 tailwind 的預設樣式, 一些 plugin: @prosemirror-adapter/vue, preset-gfm, history, clipboard, emoji, cursor, prism

---
+++ milknote/src/MilkdownEditor.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { Milkdown, useEditor } from '@milkdown/vue';
+
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+import { commonmark } from '@milkdown/preset-commonmark'
+import { gfm } from '@milkdown/preset-gfm'
+import { history } from '@milkdown/plugin-history'
+import { clipboard } from '@milkdown/plugin-clipboard'
+import { emoji } from '@milkdown/plugin-emoji'
+import { cursor } from '@milkdown/plugin-cursor'
+
+import { nord } from '@milkdown/theme-nord'
+import { prism } from '@milkdown/plugin-prism'
+
+const markdown =
+`# Milkdown Vue Commonmark
+
+> You're scared of a world where you're needed.
+
+This is a demo for using Milkdown with **Vue**.`
+
+useEditor((root) => {
+  return Editor.make()
+    .config(nord)
+    .config((ctx) => {
+      ctx.set(rootCtx, root)
+      ctx.set(defaultValueCtx, markdown)
+    })
+    .use(commonmark)
+    .use(gfm)
+    .use(history)
+    .use(clipboard)
+    .use(emoji)
+    .use(prism)
+    .use(cursor)
+})
+</script>
+
+<template>
+  <Milkdown />
+</template>
